842ed261b1ce15cc2cf96276baf0331497664d04
Zakhar Pustovgarov <deadspace1999@mail.ru>
2023-04-28 22:20:45 +0300

    Added distinction for components list

--- Assets/Scripts/ComponentCollector.cs
+++ Assets/Scripts/ComponentCollector.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using Unity.CodeEditor;
+using Unity.VisualScripting;
 using UnityEngine;
 using Component = UnityEngine.Component;
 
@@ -27,8 +28,30 @@ public class ComponentCollector : MonoBehaviour
 
         _myComponents = GetComponents<MonoBehaviour>().ToList<Component>();
 
+        MakeListDistinct();
+
         classLayout.SetComponentsList(_myComponents);
 
         classLayout.ShowList();
+    }
+
+
+    private void MakeListDistinct()
+    {
+        for(int i = 0; i < _myComponents.Count - 1; i++)
+        {
+            for(int j = i+1; j < _myComponents.Count;)
+            {
+                //Debug.Log($"{i} vs {j}");
+                if (_myComponents[i].GetType().Name == _myComponents[j].GetType().Name)
+                {
+                    _myComponents.RemoveAt(j);
+                }
+                else
+                {
+                    j++;
+                }
+            }
+        }
     }
 }
